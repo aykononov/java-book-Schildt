471f8db33f5787092daacd0f93f11c8a151fac7c
Кононов Александр Юрьевич <aykononov@yandex.ru>
2020-07-03 17:54:35 +0300

    for each

--- Chapter03/CountDayArray.java
+++ Chapter03/CountDayArray.java
@@ -1,5 +1,5 @@
 package Chapter03;
-public class AutoArray {
+public class CountDayArray {
     public static void main(String[] args) {
         int month_days[] = {31,29,31,30};
         System.out.println("Количество дней в апреле: " + month_days[3]);
@@ -7,5 +7,4 @@ public class AutoArray {
 }
 /*
 Количество дней в апреле: 30
-
  */
