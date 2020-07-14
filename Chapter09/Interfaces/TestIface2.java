52b6bf79c94faa995ea8575e27687a6817e6210a
Кононов Александр Юрьевич <aykononov@yandex.ru>
2020-07-14 13:39:12 +0300

    Chapter09

--- Chapter09/Interfaces/TestIface2.java
+++ Chapter09/Interfaces/TestIface2.java
@@ -2,6 +2,18 @@ package Chapter09.Interfaces;
 // Пример программы демонстрирует полиморфные возможности
 class TestIface2 {
     public static void main(String[] args) {
-        
+        Callback c = new Client();
+        Client2 ob = new Client2();
+
+        c.callback(42);
+
+        c = ob; // теперь переменная c ссылается на объект ob типа Client2
+        c.callback(3);
+
     }
 }
+/* -----------------------------------------
+Метод callback(), вызываемый со значением 42
+Еще один вариант метода callback()
+p в квадрате = 9
+ */
