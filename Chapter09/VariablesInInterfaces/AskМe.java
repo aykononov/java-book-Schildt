4036a2c141d94fd9b4c31db319a6b3b1c3c7015d
Кононов Александр Юрьевич <aykononov@yandex.ru>
2020-07-15 13:18:55 +0300

    Chapter09

---
+++ Chapter09/VariablesInInterfaces/AskМe.java
@@ -0,0 +1,67 @@
+package Chapter09.VariablesInInterfaces;
+import java.util.Random;
+
+// Реализация автоматизированной системы "принятия решений"
+interface SharedConstants {
+    int NO    = 0;
+    int YES   = 1;
+    int MAYBE = 2;
+    int LATER = 3;
+    int SOON  = 4;
+    int NEVER = 5;
+}
+
+class Question implements SharedConstants {
+    Random rand = new Random();
+    int ask() {
+        int prob = (int) (100 * rand.nextDouble());
+        if (prob < 30)
+            return NO;
+        else if (prob < 60)
+            return YES;
+        else if (prob < 75)
+            return LATER;
+        else if (prob < 98)
+            return SOON;
+        else
+            return NEVER;
+    }
+}
+
+class AskМe implements SharedConstants {
+    static void answer (int result) {
+        switch (result) {
+            case NO:
+                System.out.println("Нет");
+                break;
+            case YES:
+                System.out.println("Да");
+                break;
+            case MAYBE:
+                System.out.println("Возможно");
+                break;
+            case LATER:
+                System.out.println("Позже");
+                break;
+            case SOON:
+                System.out.println("Вскоре");
+                break;
+            case NEVER:
+                System.out.println("Никогда");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Question q = new Question();
+        answer(q.ask());
+        answer(q.ask());
+        answer(q.ask());
+    }
+
+}
+/* --------
+Нет
+Да
+Позже
+ */
