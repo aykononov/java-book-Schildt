b5bd1c59d9391985995a2b2846a51b103a9d6bf5
Кононов Александр Юрьевич <aykononov@yandex.ru>
2020-07-17 14:15:13 +0300

    Chapter10

---
+++ Chapter10/Exc2.java
@@ -0,0 +1,26 @@
+package Chapter10;
+
+// Пример обработки исключения
+class Exc2 {
+    public static void main(String[] args) {
+        int d, a;
+
+        try { // проконтролировать блок кода
+            d = 0;
+            a = 10 / d;
+            System.out.println("Это не будет выведено!!!");
+        } catch (ArithmeticException e) {
+            // перехватываем ошибку
+            System.out.println("ОШИБКА!!!\nДеление на нуль вызвало исключение:\n" + e);
+        }
+
+        System.out.println("\n//... код после оператора catch");
+    }
+}
+/* -------------------------------------
+ОШИБКА!!!
+Деление на нуль вызвало исключение:
+java.lang.ArithmeticException: / by zero
+
+//... код после оператора catch
+ */
