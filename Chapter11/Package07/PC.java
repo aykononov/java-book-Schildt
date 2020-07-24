731ef6208dff2ebafb8a39c98c7be4b9b561b122
Кононов Александр Юрьевич <aykononov@yandex.ru>
2020-07-24 15:05:00 +0300

    Chapter11

--- Chapter11/Package07/PC.java
+++ Chapter11/Package07/PC.java
@@ -1,4 +1,58 @@
 package Chapter11.Package07;
+
 // Неправильная реализация поставщика и потребителя
-public class PC {
+class Q {
+    int n;
+
+    synchronized int get() {
+        System.out.println("Получено: " + n);
+        return n;
+    }
+
+    synchronized void put(int n) {
+        this.n = n;
+        System.out.println("Отправлено: " + n);
+    }
+}
+
+class Producer implements Runnable {
+    Q q;
+
+    Producer(Q q) {
+        this.q = q;
+        new Thread(this, "Поставщик").start();
+    }
+
+    public void run() {
+        int i = 0;
+        while (true) {
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Q q;
+
+    Consumer(Q q) {
+        this.q = q;
+        new Thread(this, "Потребитель").start();
+    }
+
+    public void run() {
+
+        while (true) {
+            q.get();
+        }
+    }
+}
+
+class PC {
+    public static void main(String[] args) {
+        Q q = new Q();
+        new Producer(q);
+        new Consumer(q);
+
+        System.out.println("Для остановки нажмите Ctrl+C.");
+    }
 }
