2e37895dc997ced07b2d6a1997d5a0928714254a
aykononov <aykononov@yandex.ru>
2020-07-28 14:04:34 +0300

    Chapter12

--- Chapter12/Package04/AskМe.java
+++ Chapter12/Package04/AskМe.java
@@ -10,9 +10,51 @@ enum Answers {
 class Question {
     Random rand = new Random();
     Answers ask() {
-        
+        int prob = (int) (100 * rand.nextDouble());
+
+        if (prob < 15)
+            return Answers.MAYBE; // 15%
+        else if (prob < 30)
+            return Answers.NO;    // 15%
+        else if (prob < 60)
+            return Answers.YES;   // 30%
+        else if (prob < 75)
+            return Answers.LATER; // 15%
+        else if (prob < 98)
+            return Answers.SOON;  // 13%
+        else
+            return Answers.NEVER; // 2%
     }
 }
 
 class AskМe {
+    static void answer(Answers result) {
+        switch (result) {
+            case NO:
+                System.out.println("Нет");
+                break;
+            case YES:
+                System.out.println("Да");
+                break;
+            case MAYBE:
+                System.out.println("Возможно");
+                break;
+            case LATER:
+                System.out.println("Позже");
+                break;
+            case SOON:
+                System.out.println("Вскоре");
+                break;
+            case NEVER:
+                System.out.println("Никогда");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Question q = new Question();
+        for (int i = 0; i < 5; i++) {
+            answer(q.ask());
+        }
+    }
 }
