9a4588aab70854f4781b4dd2eafb014e6d61d43f
aykononov <aykononov@yandex.ru>
2020-08-04 15:18:12 +0300

    Chapter13

---
+++ Chapter13/Package02/ShowFile.java
@@ -0,0 +1,55 @@
+package Chapter13.Package02;
+
+//Отображение содержимого текстового файла
+import java.io.*;
+
+class ShowFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fis;
+
+        // Сначала проверяем, что имя файла указано
+        if (args.length != 1) {
+            System.out.println("Использование: ShowFile.main имя_файла");
+            System.out.println("Не указано: имя_файла");
+            return;
+        }
+
+        // Пробуем открыть файл
+        try {
+            fis = new FileInputStream(args[0]);
+        } catch (FileNotFoundException e) {
+            System.out.println("Heвoзмoжнo открыть файл");
+            return;
+        }
+
+        // Теперь файл открыт и готов к чтению.
+        // Далее из него читаются символы до тех пор, пока не встретится признак конца файла
+        try {
+            do {
+                i = fis.read();
+                if (i != -1) System.out.print((char) i);
+            } while (i != -1);
+        } catch (IOException e) {
+            System.out.println("Oшибкa чтения из файла");
+
+        //  если выполнение кода, в котором происходит обращение к файлу, прекращается из-за каких-нибудь исключений,
+        //  не связанных с операциями ввода-вывода, то файл все равно будет закрыт в блоке оператора finally
+        } finally {
+
+            // закрыть файл при выходе из блока оператора try
+            try {
+                fis.close();
+            } catch (IOException e) {
+                System.out.println("При ошибке закрытия файла");
+            }
+        }
+
+        // Закрываем файл
+        try {
+            fis.close();
+        } catch (IOException e) {
+            System.out.println("Oшибкa закрытия файла");
+        }
+    }
+}
