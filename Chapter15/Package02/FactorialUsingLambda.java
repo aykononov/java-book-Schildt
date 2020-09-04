e3ef0264fb2d1b810e0c485d7d08efa9c8096a02
kononov_ay <aykononov@yandex.ru>
2020-09-04 17:33:35 +0300

    new change

---
+++ Chapter15/Package02/FactorialUsingLambda.java
@@ -0,0 +1,22 @@
+package Chapter15.Package02;
+
+interface Numeric {
+    int getNum(int n);
+}
+public class FactorialUsingLambda {
+    public static void main(String[] args) {
+        Numeric factorial = (n) -> {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result = i * result;
+            } return result;
+        };
+
+        System.out.println("Факториал числа 3 равен: " + factorial.getNum(3));
+        System.out.println("Факториал числа 5 равен: " + factorial.getNum(5));
+    }
+}
+/* -------------------------
+Факториал числа 3 равен: 6
+Факториал числа 5 равен: 120
+ */
---
+++ Chapter15/Package02/StringReversalLambda.java
@@ -0,0 +1,21 @@
+package Chapter15.Package02;
+// Развернуть строку в обратном порядке (используя Лямбда-выражение)
+interface StringFunc {
+    String getReversal(String str);
+}
+public class StringReversalLambda {
+    public static void main(String[] args) {
+        StringFunc reverse = (str) -> {
+            String result = "";
+            for (int i = str.length()-1; i >= 0; i--) {
+                result += str.charAt(i);
+            }
+            return result;
+        };
+
+        System.out.println("Строка в обратном порядке : " + reverse.getReversal("Строка"));
+    }
+}
+/* -------------------------------
+Строка в обратном порядке : акортС
+ */
