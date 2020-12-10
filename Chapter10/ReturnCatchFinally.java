44580a8200d6d263ac0a559f2a83f96adae72a97
aykononov <aykononov@yandex.ru>
2020-12-10 11:55:16 +0300

    10.12.2020

--- Chapter10/ReturnCatchFinally.java
+++ Chapter10/ReturnCatchFinally.java
@@ -1,8 +1,8 @@
 package Chapter10;
+
 /* Оператор return в catch и finally */
 public class ReturnCatchFinally {
     public static void main(String[] args)  {
-        //String str = getHelloWorld();
         System.out.println(getHelloWorld());
     }
 
